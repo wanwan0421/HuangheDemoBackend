42adaef92d91c2ee652be8da584d87c06e16416d
wanwan0421 <whLi0421@gmail.com>
2025-12-04 20:17:33 +0800

    Initial commit of the backend project

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // start CORS
+  app.enableCors({
+    // allowed origins
+    origin: 'http://localhost:5173',
+    // allowed cookies
+    credentials: true,
+    // allowed methods
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    // allowed headers
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
